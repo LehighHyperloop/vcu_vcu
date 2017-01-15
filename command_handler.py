bb7f43e78c4e1136cd3bc2de56c6a9b9a2347f5d
John Ott <john.ott@johnott.us>
2017-01-14 16:08:21 -0800

    Added bad command check

--- command_handler.py
+++ command_handler.py
@@ -7,8 +7,11 @@ class CommandHandler:
     def cmd(self, msg_str):
         cmd_split = string.split(msg_str)
         if cmd_split[0] == "set":
-            target = self.ss_map[cmd_split[1]]
-            state  = cmd_split[2]
+            if cmd_split[1] in self.ss_map:
+                target = self.ss_map[cmd_split[1]]
+                state  = cmd_split[2]
 
-            if target != None:
-                target.set_target_state(state)
+                if target != None:
+                    target.set_target_state(state)
+            else:
+                print "Unknown subsystem: " + cmd_split[1]
